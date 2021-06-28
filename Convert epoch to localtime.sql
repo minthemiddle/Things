dcd5e6fefd7db7e43efeccc6b050570d327c7439
Martin Betz <martin.betz@visablelabs.com>
2021-06-28 10:47:51 +0200

    Add: All tickets sorted by creation date

--- Convert epoch to localtime.sql
+++ Convert epoch to localtime.sql
@@ -1,1 +1,24 @@
-select title, datetime(creationDate, 'unixepoch', 'localtime') from TMTask;
+-- v1
+SELECT
+	title,
+	datetime (creationDate,
+		'unixepoch',
+		'localtime')
+FROM
+	TMTask;
+
+
+-- v2
+
+with tasks_with_creation as (
+SELECT
+	title,
+	strftime('%Y-%m-%d', datetime (creationDate,
+		'unixepoch',
+		'localtime')) as creation_date
+FROM
+	TMTask
+)
+
+select * from tasks_with_creation order by creation_date desc;
+
